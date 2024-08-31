a92046798ee7dc603161adf9800e9d9c04341e77
justin4046 <71111866+justin4046@users.noreply.github.com>
2024-08-30 22:29:44 -0500

    use only mic db

--- update.sh
+++ update.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 set -e
 
-function getGIT() {
-    # getGIT $REPO $BRANCH $TARGET (directory)
-    if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]]; then echo "getGIT wrong usage, check your script or tell the author!" 1>&2; return 1; fi
-    REPO="$1"; BRANCH="$2"; TARGET="$3"; pushd .
-    if cd "$TARGET" &>/dev/null && git fetch --depth 1 origin "$BRANCH" && git reset --hard FETCH_HEAD; then popd; return 0; fi
-    if ! cd /tmp || ! rm -rf "$TARGET"; then popd; return 1; fi
-    if git clone --depth 1 --single-branch --branch "$2" "$1" "$3"; then popd; return 0; fi
-    popd; return 1;
-}
-
-getGIT https://github.com/wiedehopf/type-longnames-chrisglobe.git main "$(pwd)/longnames"
-
-if ! [[ -d "$(pwd)/longnames/individual-types" ]]; then
-    rm -rf "$(pwd)/longnames"
-    getGIT https://github.com/wiedehopf/type-longnames-chrisglobe.git main "$(pwd)/longnames"
-fi
-
 wget -O newTypes.json --compression=auto https://raw.githubusercontent.com/Mictronics/readsb-protobuf/dev/webapp/src/db/types.json
 wget -O mic-db.zip https://www.mictronics.de/aircraft-database/indexedDB_old.php
 unzip -o mic-db.zip
-
-wget -O basic-ac-db.json.gz https://downloads.adsbexchange.com/downloads/basic-ac-db.json.gz
-gunzip -c basic-ac-db.json.gz > basic-ac-db.json
-sed -i basic-ac-db.json \
-    -e 's#\\\\.##g' \
-    -e 's#\\.##g' \
-    -e 's#\\##g'
 
 function compress() {
     rm -f "$1.gz"
@@ -51,7 +27,7 @@ sed -e 's#\\u00c9#\xc3\x89#g' \
 
 perl -i -pe 's/\\u00(..)/chr(hex($1))/eg' aircraftUtf.json
 
-./toJson.py aircraftUtf.json db newTypes.json basic-ac-db.json
+./toJson.py aircraftUtf.json db newTypes.json
 
 sed -i -e 's/\\;/,/' aircraft.csv
 
